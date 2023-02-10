d7afc8f9a45d3cfe67a93fbb1986243d074c220f
Marsel <marsel5555@yandex.ru>
2023-02-10 21:27:31 +0300

    semenar1

---
+++ semenar1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1
+// Console.WriteLine("Input numbers");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Quad of {num} is {quad}");
+// Задача 2
+
+
+// Console.WriteLine("Input numbers");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (quad == num1)
+// {
+//     Console.WriteLine("First number is quad of second number");
+// }
+// else
+// {
+//     Console.WriteLine("First number is not quad of second number");
+// }
+// Задача 3
+
