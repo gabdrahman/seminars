80ab5e895378e43b09826847f55be35966ead633
Marsel <marsel5555@yandex.ru>
2023-02-12 11:18:14 +0300

    seminars

--- semenar1/Program.cs
+++ semenar1/Program.cs
@@ -23,3 +23,26 @@
 // }
 // Задача 3
 
+// Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+// Console.WriteLine("Input numbers");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+
+// while (current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// // }
+// задача 4
+
+// Console.WriteLine("Input number three- digit number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num % 10;
+
+// Console.WriteLine($"Last digit of{num} is {result}");
